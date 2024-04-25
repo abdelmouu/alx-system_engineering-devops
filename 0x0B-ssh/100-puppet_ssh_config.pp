4087f70c7ef33809993da7b33675f821079637b6
abdel  mou <abdelrebel001@gmail.com>
2024-04-24 20:10:41 -0700

    .

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,23 +1,12 @@
-# Manage the SSH client configuration file
-file { '/etc/ssh/ssh_config':
-  ensure => present,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
-}
-
-# Ensure the PasswordAuthentication option is set to 'no'
+#This puppet mainfest configures using private key ~/.ssh/school
 file_line { 'Turn off passwd auth':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-  match  => '^PasswordAuthentication',
+  line   => '    PasswordAuthentication no',
 }
 
-# Ensure the IdentityFile option is set to '~/.ssh/school'
 file_line { 'Declare identity file':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => '^IdentityFile',
+  line   => '    IdentityFile ~/.ssh/school',
 }
