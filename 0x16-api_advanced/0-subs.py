b021587741b37eacefc30cb31cbe6b9f93d09ef1
abdallah moumni <abdelrebel001@gmail.com>
2024-06-06 11:29:23 -0700

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""A module that retrieves the number of subscribers of a subreddit."""
-
-
-import requests
+"""Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit."""
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers (total subscribers)
-        for a given subreddit. If an invalid subreddit is given,
-        the function will return 0"""
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    headers = {"User-Agent": "Middle-Chipmunk-3601"}
-    response = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers=headers,
-        allow_redirects=False
-    )
-    if response.status_code == 200:
-        result = response.json().get("data")
-        return result.get("subscribers")
-    else:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
+
+    return sub_info.json().get("data").get("subscribers")
