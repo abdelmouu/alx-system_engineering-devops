813c54f2df1e61df530cf4aee66260ec9399b4ad
abdallah moumni <abdelrebel001@gmail.com>
2024-06-06 10:56:52 -0700

    .

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
 """
-A Script that, uses this REST API, for a given employee ID, returns
+A Script that, using this REST API, for a given employee ID, returns
 information about his/her TODO list progress
 """
-
-import json
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        exit(1)
 
-    sessionReq = requests.Session()
+    employee_id = argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url)
 
-    idEmp = argv[1]
-    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
-    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+    if response.status_code == 200:
+        employee_data = response.json()
+        employee_name = employee_data["name"]
 
-    employee = sessionReq.get(idURL)
-    employeeName = sessionReq.get(nameURL)
+        todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+        todos_response = requests.get(todos_url)
 
-    json_req = employee.json()
-    name = employeeName.json()['name']
+        if todos_response.status_code == 200:
+            todos = todos_response.json()
+            total_tasks = len(todos)
+            done_tasks = sum(1 for task in todos if task["completed"])
 
-    totalTasks = 0
-
-    for done_tasks in json_req:
-        if done_tasks['completed']:
-            totalTasks += 1
-
-    print("Employee {} is done with tasks({}/{}):".
-          format(name, totalTasks, len(json_req)))
-
-    for done_tasks in json_req:
-        if done_tasks['completed']:
-            print("\t " + done_tasks.get('title'))
+            print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
+            for task in todos:
+                if task["completed"]:
+                    print(f"\t {task['title']}")
+        else:
+            print("Failed to retrieve TODO list.")
+    else:
+        print("Failed to retrieve employee data.")
